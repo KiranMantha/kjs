278d212900ef720d23ae22150e80d7886e1b83cc
Venkata Satya Sai Kiran Mantha <vmantha@evoketechnologies.com>
2018-03-23 17:43:23 +0530

    code refractoring

--- index.js
+++ index.js
@@ -57,7 +57,7 @@
 
 		var bindContext = function(context){
 			try { 
-				result = new Function('obj', code).call(context); 
+				result = new Function(code).call(context); 
 			}
 			catch(err) { 
 				console.error("'" + err.message + "'", " in \n\nCode:\n", code, "\n"); 
