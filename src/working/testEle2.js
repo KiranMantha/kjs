5ae0386b3d93cd27696c6f82f094104d5317e919
Venkata Satya Sai Kiran Mantha <vmantha@evoketechnologies.com>
2018-06-08 19:43:20 +0530

    working with nested routes

--- src/working/testEle2.js
+++ src/working/testEle2.js
@@ -46,7 +46,7 @@ export default class TestElem extends Component {
         <div>
           <ul>
             <li><router-link path='/home' displayName='router link1' render='TestElement'></router-link></li>
-            <li><router-link path='/test' displayName='router link2' render='TestElement'></router-link></li>
+            <li><router-link path='/test' displayName='router link2' render='TestElem'></router-link></li>
           </ul>
           <router-outlet></router-outlet>
         </div>
