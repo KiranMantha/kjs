b476a577bc5b7b8be0516a9bf9e6a9008a3ed26f
KiranMantha <murali-evoke@gmail.com>
2018-08-08 23:20:21 +0530

    inprogress

--- src/litete/registerComponent.js
+++ src/litete/registerComponent.js
@@ -1,16 +1,14 @@
 import get from "lodash/get";
-import forEach from "lodash/forEach";
-import kebabCase from "lodash/kebabCase";
 
 const registry = (() => {
     let _registry = {};
 
     let _get = (componentName) => {
-        return get(_registry, `lte.${componentName}`, null);
+        return get(_registry, `lte-${componentName}`, null);
     }
 
     let _set = (tag, component) => {
-        let name = `lte.${tag}`;
+        let name = `lte-${tag}`;
         if (!_registry[name]) {
             _registry[name] = component;
         }
--- src/litete/vdomToHtml.js
+++ src/litete/vdomToHtml.js
@@ -1,88 +1,223 @@
-import { findFirst } from 'obj-traverse/lib/obj-traverse';
-import get from 'lodash/get';
-import forEach from 'lodash/forEach';
-import registry from './registerComponent';
-import attachDomEvents from './domEvents';
-import ComponentFactory from './componentFactory';
+import registry from '../registry';
 
-function traverse(json, keyName) {
-    let value = '';
-    if (Array.isArray(json)) {
-        json.forEach(traverse);
-    } else if (typeof json === 'object') {
-        Object.keys(json).forEach(function(key) {
-            if (key === keyName) {
-                value = json[key];
-                return;
-            } else {
-               traverse(json[key]);
-            }
-        });
+const get = (obj, path, defaultValue) => {
+    let patharr = path.trim().split('.');
+    let value;
+    let k;
+    for (let i of patharr) {
+        k = k ? k[i] : obj[i];
+        if (k && typeof k !== 'object') {
+            value = k;
+            return value;
+        }
+    }
+    if (typeof value === 'undefined') {
+        if (typeof defaultValue !== 'undefined')
+            value = defaultValue
+        else
+            value = '';
     }
     return value;
-};
-
-const getHtmlFromVDom = (vdom, parentNode, context) => {
-    let isEvent = /^on/;
-    let isExpression = /{([^{}]+)}/g;
-    let hasBind = /.bind(.*?)/g;
-    let match;
-    if(!context) context = {};
-    
-    let nodeCreated = ComponentFactory.createComponentNode(null, vdom, parentNode, context);
-
-    if (!nodeCreated) {
-        let node = document.createElement(vdom.type);
-        node._vdom = vdom;
-        //Add properties
-        for (let prop in vdom.props) {
-            if (prop !== 'children') {
-                let listener;
-                let propValue = vdom.props[prop];
-                if (prop.match(isEvent)) {
-                    if (match = hasBind.exec(propValue)) {
-                        //listener = (new Function('context', 'prop', 'traverse', `return traverse(context, prop)`))(context, `${vdom.props[prop]}`, traverse);
-                        listener = (new Function('context', `return context.${propValue}`))(context);
-                    } else {
-                        listener = context[propValue]
-                    }
-                    node.addEventListener(prop.substring(2), listener, false);
-                } else if (match = isExpression.exec(propValue)) {
-                    let _value = get(context, match[1].trim(), '');
-                    //let _value = traverse(context, match[1].trim());
-                    propValue = _value;
-                    node.setAttribute(prop, propValue);
-                } else {
-                    node.setAttribute(prop, propValue);
-                }
-            }
-        }
-
-        //Append Children Nodes
-        for (let child of vdom.children) {
-            if (typeof child === 'string') {
-                let found = [];
-                while (match = isExpression.exec(child)) {
-                    found.push(match[1]);
-                }
-                if (found.length > 0) {
-                    forEach(found, (val) => {
-                        let value = get(context, val.trim(), '');
-                        let textnode = document.createTextNode(value);
-                        node.appendChild(textnode);
-                    });
-                } else {
-                    let textnode = document.createTextNode(child);
-                    node.appendChild(textnode);
-                }
-            } else {
-                getHtmlFromVDom(child, node, context);
-            }
-        }
-        if(parentNode.contains(node)) parentNode.removeChild(node);
-        attachDomEvents(node);
-        parentNode.appendChild(node);
-    }    
 }
 
-export default getHtmlFromVDom;
+//export default get;
+
+//regex
+let forRegex = /^\s*([\s\S]+?)\s+in\s+([\s\S]+?)(?:\s+as\s+([\s\S]+?))?(?:\s+track\s+by\s+([\s\S]+?))?\s*$/,
+    isExpression = /{{(.+?)}}/g,
+    isEvent = /^on(Click|Submit|Input|Change)/,
+    isFuncWithArgs = /\(\s*([^)]+?)\s*\)/,
+    getFuncName = /^\s*[A-Za-z][A-Za-z0-9_]*([^\(]*)/i;
+
+
+//Props
+function setProp($target, name, value) {
+    if($target.props) {
+        $target.props[name] = value;
+    } else if (isCustomProp(name)) {
+        return;
+    } else if (typeof value === 'boolean') {
+        setBooleanProp($target, name, value);
+    } else {
+        $target.setAttribute(name, value);
+    }
+}
+
+function setBooleanProp($target, name, value) {
+    if (value) {
+        $target.setAttribute(name, value);
+        $target[name] = true;
+    } else {
+        $target[name] = false;
+    }
+}
+
+function removeBooleanProp($target, name) {
+    $target.removeAttribute(name);
+    $target[name] = false;
+}
+
+function removeProp($target, name, value) {
+    if (isCustomProp(name)) {
+        return;
+    } else if (typeof value === 'boolean') {
+        removeBooleanProp($target, name);
+    } else {
+        $target.removeAttribute(name);
+    }
+}
+
+function setProps(context, $target, props) {
+    let len = $target.localName.split('-').length;
+    if (len > 1) {
+        $target.props = {};
+        $target.attrs = props;
+    }
+    Object.keys(props).forEach(name => {
+        let value;
+        if ($target.props) {
+            value = get(context, props[name]);
+        } else {
+            value = props[name];
+        }
+        setProp($target, name, value);
+    });
+
+}
+
+function isCustomProp(name) {
+    return isEventProp(name) || name === 'forceUpdate';
+}
+
+function getContextProps(context, props) {
+    let $props = {};
+    for (let prop in props) {
+        $props[prop] = get(context, arr[prop]);
+    }
+    return $props;
+}
+
+function updateProp(context, $target, name, newVal, oldVal) {
+    if (!newVal) {
+        removeProp($target, name, oldVal);
+    } else if (!oldVal || newVal !== oldVal) {
+        setProp(context, $target, name, newVal);
+    }
+}
+
+function updateProps(context, $target, newProps, oldProps = {}) {
+    if ($target.isLiteComponent) {
+        let props = {};
+        let arr = $target.attrs;
+        for (let prop in arr) {
+            props[prop] = get(context, arr[prop]);
+        }
+        $target.setProps(props);
+    } else {
+        const props = Object.assign({}, newProps, oldProps);
+        Object.keys(props).forEach(name => {
+            updateProp(context, $target, name, newProps[name], oldProps[name]);
+        });
+    }
+}
+
+//Events
+var events = {
+    'onClick': 'click',
+    'onSubmit': 'submit'
+}
+function isEventProp(name) {
+    return /^on/.test(name);
+}
+
+function extractEventName(name) {
+    return name.slice(2).toLowerCase();
+}
+
+function addEventListeners(context, $target, props) {
+    let match;
+    let match1;
+    for (let prop in props) {
+        if (match = isEvent.exec(prop)) {
+            let prop_value = props[prop];
+            if (match1 = isFuncWithArgs.exec(prop_value)) {
+                let args = match1[1].split(',');
+                let values = [];
+                for (let item of args) {
+                    values.push(get(context, item));
+                }
+                let funcName = getFuncName.exec(value)[0];
+                node.addEventListener(events[match[0]], get(context, funcName).bind(context, ...values), false);
+            } else {
+                $target.addEventListener(events[match[0]], get(context, prop_value).bind(context), false);
+            }
+            $target.eventsBinded = true;
+        }
+    };
+}
+
+//DOM check
+function createElement(context, node) {
+    let $el;
+    let $comp = registry.getComponent(node.type);
+    if (typeof node === 'string') {
+        return document.createTextNode(node);
+    }
+    if(!$comp) {
+        $el = document.createElement(node.type);
+        setProps(context, $el, node.props);
+        addEventListeners(context, $el, node.props);
+    } else {
+        let $props = getContextProps(context, node.props);
+        $el = new $comp($props);
+    }
+    
+    node.children
+        .map(x => createElement(context, x))
+        .forEach($el.appendChild.bind($el));
+    return $el;
+}
+
+function changed(node1, node2) {
+    return typeof node1 !== typeof node2 ||
+        typeof node1 === 'string' && node1 !== node2 ||
+        node1.type !== node2.type ||
+        (node1.props && node1.props.forceUpdate);
+}
+
+function updateElement(context, $parent, newNode, oldNode, index = 0) {
+    if (!oldNode) {
+        $parent.appendChild(
+            createElement(context, newNode)
+        );
+    } else if (!newNode) {
+        $parent.removeChild(
+            $parent.childNodes[index]
+        );
+    } else if (changed(newNode, oldNode)) {
+        $parent.replaceChild(
+            createElement(context, newNode),
+            $parent.childNodes[index]
+        );
+    } else if (newNode.type) {
+        updateProps(
+            context,
+            $parent.childNodes[index],
+            newNode.props,
+            oldNode.props
+        );
+        const newLength = newNode.children.length;
+        const oldLength = oldNode.children.length;
+        for (let i = 0; i < newLength || i < oldLength; i++) {
+            updateElement(
+                context,
+                $parent.childNodes[index],
+                newNode.children[i],
+                oldNode.children[i],
+                i
+            );
+        }
+    }
+}
+
+export { createElement, changed, updateElement, h };
